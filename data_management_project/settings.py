bdcdcf78110730d3dddaf29e64286edd4942e7c2
Massan Sarsenbayev <100170921+msarsenbayev@users.noreply.github.com>
2023-05-01 21:56:22 +0100

    second commit

--- data_management_project/settings.py
+++ data_management_project/settings.py
@@ -109,14 +109,14 @@ WSGI_APPLICATION = 'data_management_project.wsgi.application'
 # }
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Qazanbasbala#1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+#    'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Qazanbasbala#1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
 }
 
 
