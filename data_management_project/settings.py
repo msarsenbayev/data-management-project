9894e5b2ee7004ae19119e514d3e981b3fad7af2
Massan Sarsenbayev <100170921+msarsenbayev@users.noreply.github.com>
2023-05-01 21:59:54 +0100

    first commit

--- data_management_project/settings.py
+++ data_management_project/settings.py
@@ -101,14 +101,14 @@ WSGI_APPLICATION = 'data_management_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
 #    'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'postgres',
@@ -117,7 +117,7 @@ DATABASES = {
 #         'HOST': '127.0.0.1',
 #         'PORT': '5432',
 #     }
-}
+# }
 
 
 # Password validation
