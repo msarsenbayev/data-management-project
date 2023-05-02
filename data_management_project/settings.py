b6af5bf5c6f0566974e32cdafae3a5a99c87ebf6
Massan Sarsenbayev <100170921+msarsenbayev@users.noreply.github.com>
2023-05-02 23:43:19 +0100

    commite

--- data_management_project/settings.py
+++ data_management_project/settings.py
@@ -122,16 +122,16 @@ DATABASES = {
 #     }
 # }
 
-# MAX_CONN_AGE = 600
+MAX_CONN_AGE = 600
 
-# if "DATABASE_URL" in os.environ:
-#     # Configure Django for DATABASE_URL environment variable.
-#     DATABASES["default"] = dj_database_url.config(
-#         conn_max_age=MAX_CONN_AGE, ssl_require=True)
+if "DATABASE_URL" in os.environ:
+    # Configure Django for DATABASE_URL environment variable.
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
 
-#     # Enable test database if found in CI environment.
-#     if "CI" in os.environ:
-#         DATABASES["default"]["TEST"] = DATABASES["default"]
+    # Enable test database if found in CI environment.
+    if "CI" in os.environ:
+        DATABASES["default"]["TEST"] = DATABASES["default"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
